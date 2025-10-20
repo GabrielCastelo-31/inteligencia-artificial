f243216376d21227a242c02c5485dd5867d9c0e1
Gabriel_Castelo <castelogabriel@outlook.com>
2025-10-20 17:30:22 -0300

    melhora organização do arquivo e adiciona random_restart

--- hill_climbing.py
+++ hill_climbing.py
@@ -2,8 +2,10 @@ import random
 import matplotlib.pyplot as plt
 
 STEP_SIZE = 0.01  # escala entre pontos para buscar
-MAX_STEPS = 10000  # numero de pontos a serem avaliados
+MAX_STEPS = 100000  # numero de pontos a serem avaliados
 INTERVAL_SIZE = 20  # intervalo de busca
+MAX_RANDOM_RESTARTS = 5  # numero de tentativas com pontos iniciais aleatorios
+
 
 # Função para maximizar
 
@@ -69,8 +71,49 @@ def linspace(x) -> list[int]:
     return points
 
 
+def random_restart_hill_climbing(user_input=2) -> int:
+    """ Executa o algoritmo de Hill Climbing com reinícios aleatórios.
+    Retorna o melhor resultado encontrado.
+
+    """
+    best_y = None
+    best_x = None
+    best_iteration = None
+
+    if user_input == 1:
+        print("Iniciando Hill Climbing para minimização...")
+    else:
+        print("Iniciando Hill Climbing para maximização...")
+    for i in range(MAX_RANDOM_RESTARTS):
+        start = random.random() * 100 - 50  # ponto inicial aleatório entre -50 e 50
+        print('Ponto inicial: ', start)
+
+        ########### Minimização ############
+        if user_input == 1:
+            x, y = hill_climb(start, min=True)
+            best_y = y if best_y is None else min(best_y, y)
+            best_x = x if best_x is None else (x if y == best_y else best_x)
+
+        else:
+            ########### Maximização ############
+            x, y = hill_climb(start, min=False)
+            best_y = y if best_y is None else max(best_y, y)
+            best_x = x if best_x is None else (x if y == best_y else best_x)
+
+        best_iteration = i if best_iteration is None else (
+            i if y == best_y else best_iteration)
+        print('iteração ', i, ', x: ', x, ' ', 'y: ', y)
+    return best_x, best_y, best_iteration
+
+
 # print(linspace(0))
-start = 10
-x, y = hill_climb(start, min=True)  # minimizar
-# x, y = hill_climb(start, min=False)  # maximizar
-print('melhor x: ', x, ' ', 'melhor y: ', y)
+user_input = int(input(
+    "Hill Climbing com reinícios aleatórios. Digite 1 para minimizar ou 2 para maximizar: "))
+
+best_x, best_y, best_iteration = random_restart_hill_climbing(user_input)
+if best_x is not None and best_y is not None:
+
+    print(
+        f'Melhor resultado encontrado: x = {best_x}, y = {best_y} na iteração {best_iteration}')
+else:
+    print('Nenhum resultado encontrado.')
